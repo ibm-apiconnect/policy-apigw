4b35fd15671e8a158bd6d9ba2c141998e260147f
Tom Van Oppens <4301313+tomvanoppens@users.noreply.github.com>
2022-11-25 12:21:07 +0100

    update mismatch parameter name
    Signed-off-by: Tom Van Oppens <4301313+tomvanoppens@users.noreply.github.com>

--- user-defined-policies/ip-allowlist/ip-limit.js
+++ user-defined-policies/ip-allowlist/ip-limit.js
@@ -1,14 +1,15 @@
-const ranges = JSON.parse(context.get("local.parameter.ip-limit"));
-console.error(ranges)
+console.error(context.get("local.parameter.ranges"))
+const ranges = JSON.parse(context.get("local.parameter.ranges"));
+console.debug(ranges)
 
 const ip = context.get('message.headers.X-Client-Ip');
-console.error("client ip:" + ip)
+console.debug("client ip:" + ip)
 
 const inRange = ranges.some(
   ([min, max]) => IPtoNum(min) < IPtoNum(ip) && IPtoNum(max) > IPtoNum(ip)
 );
 
-console.error(inRange);
+console.debug(inRange);
 if(inRange){
     context.reject('IPError', 'Your IP is not allowed to make this API');
     context.message.statusCode = '403 Frobidden';
